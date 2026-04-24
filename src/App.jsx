4e77891f01036dce92f694e9ba50b3a140692b54
smontan <sherwinmontanez3@gmail.com>
2026-04-24 16:00:57 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Welcome to my macfolio</h1>
+      App
+    </div>
+  );
+};
+
+export default App;
